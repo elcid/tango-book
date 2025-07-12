13f88436488bf8e29417ff541d461b41a52a1ee2
JRReyesM <jreyesm@proton.me>
2025-07-12 18:13:05 +0200

    Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site: 'https://elcid.github.io',
+  base: '/tango-book',
 
-  integrations: [react()]
 });
